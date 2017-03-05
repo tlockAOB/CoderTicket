4a4727c496e7a3352c4bfe06e70261c53b068ebf
tlockAOB <tlock@armyofbees.com>
2017-03-05 11:56:42 +0700

    write two more route tests

--- spec/routes/incoming_spec.rb
+++ spec/routes/incoming_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "routing to /upcoming", type: 'routing' do
+RSpec.describe "events routing", type: 'routing' do
 
-  it "/upcoming routes to events#index" do
-    expect(get: '/upcoming').to route_to(controller: 'events', action: 'index')
+  it "/events routes to events#index" do
+    expect(get: '/events').to route_to(controller: 'events', action: 'index')
+  end
+
+  it "events_path routes to events#index" do
+    expect(get: events_path).to route_to(controller: 'events', action: 'index')
+  end
+
+  it "events_path with search routes to events#index" do
+    expect(get: events_path(search: 'Something')).to route_to(controller: 'events', action: 'index', search: 'Something')
   end
 
 end
