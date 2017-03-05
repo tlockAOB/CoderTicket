f3f0e200d5b9d6f8494fa4a5cc11f2af7a7473da
tlockAOB <tlock@armyofbees.com>
2017-03-05 19:20:58 +0700

    user can create events though events/new

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,4 +14,21 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+
+  def create
+   @event = Event.new event_params
+    if @event.save
+      flash[:success] = "Event Created, Ready for publish"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:creator_id, :extended_html_description, :venue_id, :category_id, :starts_at, :ends_at, :name)
+  end
+
 end
